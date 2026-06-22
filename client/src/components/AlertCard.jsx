b59c8d663adf6a8e7a9173ed0afeb9e397809287
Shudhanshu <shudhanshusingh649@gmail.com>
2026-06-22 18:57:29 +0530

    Process IMD rainfall, maximum temperature and minimum temperature datasets (2010-2025)

--- client/src/components/AlertCard.jsx
+++
@@ -1,7 +0,0 @@
-function AlertCard({ zone, risk }) {
-  return (
-    <div className="bg-red-50 border-l-4 border-red-500 p-4">
-      {zone} - {risk}
-    </div>
-  );
-}
--- client/src/components/RecommendationCard.jsx
+++
@@ -1,7 +0,0 @@
-function RecommendationCard({ title }) {
-  return (
-    <div className="bg-green-50 border p-4 rounded-lg">
-      ✓ {title}
-    </div>
-  );
-}
