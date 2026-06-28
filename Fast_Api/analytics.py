559afeb918d1566a8fcec28a2a8418b21b842edb
Shudhanshu <shudhanshusingh649@gmail.com>
2026-06-28 13:58:08 +0530

    Fixed analytics API and optimized dataset loading

--- Fast_Api/analytics.py
+++ Fast_Api/analytics.py
@@ -2,20 +2,20 @@ import pandas as pd
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 DATA_PATH = BASE_DIR / "Final_Dataset" / "climate_data_2010_2025.csv"
 
-# CSV sirf ek baar load hogi
-df = pd.read_csv(DATA_PATH)
-
 def get_analytics():
+
+    df = pd.read_csv(DATA_PATH)
+
+    # Date se Year nikalo
+    df["Date"] = pd.to_datetime(df["Date"])
+    df["Year"] = df["Date"].dt.year
 
     total_records = len(df)
 
     avg_rainfall = round(df["Rainfall"].mean(), 2)
-
     avg_max_temp = round(df["Max_Temperature"].mean(), 2)
-
     avg_min_temp = round(df["Min_Temperature"].mean(), 2)
 
     hottest_year = (
@@ -31,25 +31,14 @@ def get_analytics():
     )
 
     return {
-
         "total_records": total_records,
-
         "date_range": {
-
-            "start": str(df["Date"].min()),
-
-            "end": str(df["Date"].max())
-
+            "start": str(df["Date"].min().date()),
+            "end": str(df["Date"].max().date())
         },
-
         "average_rainfall": avg_rainfall,
-
         "average_max_temperature": avg_max_temp,
-
         "average_min_temperature": avg_min_temp,
-
         "hottest_year": int(hottest_year),
-
         "wettest_year": int(wettest_year)
-
     }
